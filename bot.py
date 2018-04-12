84ffb67138369b0f6b3584f4d6392aec030f2b4b
Upsehaj Singh Makkar <upsehaj@gmail.com>
2018-04-13 00:43:03 +0530

    Incoming and Outgoing Working Now

--- bot.py
+++ bot.py
@@ -1,10 +1,6 @@
 import requests
 import datetime
-from time import sleep
-
-file = open('../tok.txt', 'r')
-data = file.readlines()
-token = data[0]
+import os
 
 class BotHandler:
 
@@ -16,6 +12,7 @@ class BotHandler:
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
+        print(resp.json())
         result_json = resp.json()['result']
         return result_json
 
@@ -28,34 +25,40 @@ class BotHandler:
     def get_last_update(self):
         get_result = self.get_updates()
         if len(get_result) <= 0:
-            last_update = get_result[-1]
+            last_update = -1
         else:
-            last_update = get_result[len(get_result)]
+            last_update = get_result[len(get_result)-1]
         return last_update
 
+pwd = os.path.dirname(os.path.abspath(__file__))
+tok = open("{}/../tok.txt".format(pwd), "r")
+data = tok.readlines()
+token = data[0]
 barca_bot = BotHandler(token)  
 now = datetime.datetime.now()
 
 def main():  
 
     new_offset = None
-    today = now.day
-    hour = now.hour
 
     while True:
         barca_bot.get_updates(new_offset)
         last_update = barca_bot.get_last_update()
+        if last_update == -1:
+            continue
         last_update_id = last_update['update_id']
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
         # last_chat_name = last_update['message']['chat']['first_name']
 
+        now = datetime.datetime.now()
+
         if last_chat_text.lower() == 'score':
+            text = 'this is score'
             barca_bot.send_message(last_chat_id, text)
-            today += 1
         elif last_chat_text.lower() == 'fix':
+            text = 'this is fix'
             barca_bot.send_message(last_chat_id, text)
-            today += 1
         
         new_offset = last_update_id + 1
 
@@ -64,3 +67,10 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         exit()
+'''
+score on demand
+fixtures on demand
+score after every 15min
+score whenever a goal scored
+notify a day before the match 
+'''
