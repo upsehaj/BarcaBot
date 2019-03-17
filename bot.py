db3185d403579bdfaea0d76386e2556d433d5974
Upsehaj <upsehaj@gmail.com>
2019-03-18 02:16:41 +0530

    minor

--- bot.py
+++ bot.py
@@ -263,8 +263,9 @@ def send_updates_reminder():
             text = ''
             for fixture in fixtures:
                 if (fixture['status'] == 'IN_PLAY' or fixture['status'] == 'PAUSED') and fixture['competition']['id'] in codes:
-                    last_home_goal = db.execute("SELECT last_home_goal from scores")[0][0]
-                    last_away_goal = db.execute("SELECT last_away_goal from scores")[0][0]
+                    goals = db.execute("SELECT * from scores")
+                    last_home_goal = goals[0][0]
+                    last_away_goal = goals[0][1]
                     if fixture['score']['fullTime']['homeTeam'] == last_home_goal and fixture['score']['fullTime']['awayTeam'] == last_away_goal:
                         break
                     text = text + '{}\n'.format(codes[fixture['competition']['id']])
